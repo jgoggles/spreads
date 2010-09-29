ea0011282117a81ca5967716118269193e658dac
Dan Weaver <dweaver@denvernewspaperagency.com>
2010-09-29 14:14:35 -0600

    sportsinteraction fix

--- app/models/game.rb
+++ app/models/game.rb
@@ -93,23 +93,32 @@ class Game < ActiveRecord::Base
 
       if rows
         rows.each do |a|
-          matchup = a.at_css('span.title a').content.match(/(.*)\sat\s(.*)/)
-          away = $1.strip!
-          home = $2.strip!
+          if !a.at_css('div ul[2] li[2] span span.handicap').nil?
+            puts "jo"
+            matchup = a.at_css('span.title a').content.match(/(.*)\sat\s(.*)/)
+            away = $1.strip!
+            home = $2.strip!
 
-          away.gsub!(/\sJets|\sGiants/, "")
-          home.gsub!(/\sJets|\sGiants/, "")
-          
-          line = a.at_css('div ul[2] li[2] span span.handicap').content
+            away.gsub!(/\sJets|\sGiants/, "")
+            home.gsub!(/\sJets|\sGiants/, "")
+        
+            line = a.at_css('div ul[2] li[2] span span.handicap').content
 
-          lines.push(Hash.new)
-          lines[rows.index(a)]['game'] = {}
-          lines[rows.index(a)]['game']['home'] = home
-          lines[rows.index(a)]['game']['away'] = away
-          lines[rows.index(a)]['game']['line'] = line.strip!
+            lines.push(Hash.new)
+            lines[rows.index(a)]['game'] = {}
+            lines[rows.index(a)]['game']['home'] = home
+            lines[rows.index(a)]['game']['away'] = away
+            lines[rows.index(a)]['game']['line'] = line.strip!
+          else
+            lines.push(Hash.new)
+            lines[rows.index(a)]['game'] = {}
+            lines[rows.index(a)]['game']['home'] = "n/a"
+            lines[rows.index(a)]['game']['away'] = "n/a"
+            lines[rows.index(a)]['game']['line'] = "n/a"
+          end
         end
       end
-      puts lines
+      
       return lines
     rescue Exception => e
       print e, "\n"

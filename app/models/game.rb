1bba0ceddd79f3281f7e953c8986605fe778b457
Dan Weaver <dweaver@denvernewspaperagency.com>
2010-10-07 13:46:27 -0600

    stupid fucking people

--- app/models/game.rb
+++ app/models/game.rb
@@ -109,8 +109,10 @@ class Game < ActiveRecord::Base
             lines[rows.index(a)]['game'] = {}
             lines[rows.index(a)]['game']['home'] = home
             lines[rows.index(a)]['game']['away'] = away
-            if line.strip.size == 2 
+            if line.strip.size == 2 && a.at_css('div ul[2] li[2] span span.price').content.strip.size == 2
               lines[rows.index(a)]['game']['line'] = "n/a"
+            elsif line.strip.size == 2 && a.at_css('div ul[2] li[2] span span.price').content.strip.size > 2
+              lines[rows.index(a)]['game']['line'] = "0"
             else
               lines[rows.index(a)]['game']['line'] = line.strip!
             end

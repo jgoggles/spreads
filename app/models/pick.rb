11598402773db3cebf94468302140caa99251b41
Dan <daniel.weaver@gmail.com>
2010-11-22 07:57:25 -0700

    blah

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -12,11 +12,11 @@ class Pick < ActiveRecord::Base
 
   validate :pick_time
 
-  def pick_time
-    unless self.game_id.nil?
-      errors.add(:base, "Game has already started. Please pick a different game.") if Time.now > Game.find(self.game_id).date
-    end
-  end
+#  def pick_time
+#    unless self.game_id.nil?
+#      errors.add(:base, "Game has already started. Please pick a different game.") if Time.now > Game.find(self.game_id).date
+#    end
+#  end
 
   def team
     game = Game.find(self.game_id)
